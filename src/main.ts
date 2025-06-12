c1307df405c8420d8abdb445456b30b87e980422
Jalal Manafi <53921355+jalalmanafi@users.noreply.github.com>
2025-06-12 16:12:30 +0400

    🐛 Removed import line from main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import typescriptLogo from "./typescript.svg";
 
 import "./style.css";
 
-import "./array/pop/pop";
-
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div class="main">
     <a href="https://www.typescriptlang.org/" target="_blank">
